bf1f4b9851ebcdb7425cb09a5227eb186bd7754e
jum2py <65761213+meeny221@users.noreply.github.com>
2022-08-31 16:58:35 +0900

    202 done
    take class until 202

---
+++ app.js
@@ -0,0 +1,15 @@
+// alert("hi");
+console.log(545454545);
+console.log("hello there");
+
+const a = 8;
+const b = 2;
+// this means constant, 바뀌지 않는 값.
+
+const myName = "Allen";
+// in the JS world, if we need space, we use camelCase
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
