fe6a2305c02b02bab1627d9e8f25f688227869ae
jum2py <65761213+meeny221@users.noreply.github.com>
2022-09-09 16:28:23 +0900

    update 204

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 console.log(545454545);
 console.log("hello there");
 
-const a = 8;
+let a = 8;
 const b = 2;
-// this means constant, 바뀌지 않는 값.
+// const means constant, and constant can not be changed.
 
-const myName = "Allen";
+let myName = "Allen";
 // in the JS world, if we need space, we use camelCase
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "Seungjin";
+// let is when I want for create a New one.
+// Once, let exists if I want it to update, I just update.
+console.log("Your new name is " + myName);
